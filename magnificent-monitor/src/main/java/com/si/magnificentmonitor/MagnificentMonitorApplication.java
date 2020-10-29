d08340c134c9fba248f22bc78f7f00c8664c7475
Simon Schäfer <simon.schaefer@campus.tu-berlin.de>
2020-10-29 13:00:19 +0100

    allows to manage schedulign support through config

--- magnificent-monitor/src/main/java/com/si/magnificentmonitor/MagnificentMonitorApplication.java
+++ magnificent-monitor/src/main/java/com/si/magnificentmonitor/MagnificentMonitorApplication.java
@@ -2,6 +2,9 @@ package com.si.magnificentmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class MagnificentMonitorApplication {
@@ -10,4 +13,10 @@ public class MagnificentMonitorApplication {
 		SpringApplication.run(MagnificentMonitorApplication.class, args);
 	}
 
+
+	@ConditionalOnProperty(value = "monitor.scheduling.enabled", havingValue = "true")
+	@Configuration
+	@EnableScheduling
+	public static class SchedulingConfiguration {
+	}
 }
